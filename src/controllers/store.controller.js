d1801d42164d0f3ef21609650fa7df38d5ff0d4d
HaSam <ssam8610@naver.com>
2025-11-24 00:14:19 +0900

    8주차 swagger 실습

--- src/controllers/store.controller.js
+++ src/controllers/store.controller.js
@@ -4,6 +4,41 @@ import { bodyToReview } from "../dtos/review.dto.js";
 import { createReview } from "../services/review.service.js";
 
 export const handleAddStoreReview = async (req, res, next) => {
+    /*
+    #swagger.summary = '상점 리뷰 목록 조회 API';
+    #swagger.responses[200] = {
+      description: "상점 리뷰 목록 조회 성공 응답",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              resultType: { type: "string", example: "SUCCESS" },
+              error: { type: "object", nullable: true, example: null },
+              success: {
+                type: "object",
+                properties: {
+                  data: {
+                    type: "array",
+                    items: {
+                      type: "object",
+                      properties: {
+                        id: { type: "number" },
+                        store: { type: "object", properties: { id: { type: "number" }, name: { type: "string" } } },
+                        user: { type: "object", properties: { id: { type: "number" }, email: { type: "string" }, name: { type: "string" } } },
+                        content: { type: "string" }
+                      }
+                    }
+                  },
+                  pagination: { type: "object", properties: { cursor: { type: "number", nullable: true } }}
+                }
+              }
+            }
+          }
+        }
+      }
+    };
+  */
       console.log("리뷰작성을 요청했습니다!");
       console.log("body:", req.body); // 값이 잘 들어오나 확인하기 위한 테스트용
     
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { handleUserSignUp, startUserMission, handleListMyReviews, handleListMyMi
 import { handleListStoreReviews, handleAddStoreReview, handleListStoreMissions } from "./controllers/store.controller.js"
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import swaggerAutogen  from "swagger-autogen";
+import swaggerUiExpress from "swagger-ui-express";
 
 dotenv.config();
 
@@ -12,6 +14,37 @@ const app = express();
 app.use(morgan('dev'))
 app.use(cookieParser());
 const port = process.env.PORT;
+
+app.use(
+  "/docs",
+  swaggerUiExpress.serve,
+  swaggerUiExpress.setup({}, {
+    swaggerOptions: {
+      url: "/openapi.json",
+    },
+  })
+);
+
+app.get("/openapi.json", async (req, res, next) => {
+  // #swagger.ignore = true
+  const options = {
+    openapi: "3.0.0",
+    disableLogs: true,
+    writeOutputFile: false,
+  };
+  const outputFile = "/dev/null"; // 파일 출력은 사용하지 않습니다.
+  const routes = ["./src/index.js"];
+  const doc = {
+    info: {
+      title: "UMC 9th",
+      description: "UMC 9th Node.js 테스트 프로젝트입니다.",
+    },
+    host: "localhost:3000",
+  };
+
+  const result = await swaggerAutogen(options)(outputFile, routes, doc);
+  res.json(result ? result.data : null);
+});
 
 /**
  * 공통 응답을 사용할 수 있는 헬퍼 함수 등록
